773bd4f417daac3c0341ae40f3154646460cf410
Manuel0311Nan <manuzz3112@gmail.com>
2025-01-05 11:40:47 +0100

    second commit, auth;user

---
+++ src/Controller/Admin/BookCrudController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Book;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+class BookCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Book::class;
+    }
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id')->hideOnForm(),
+            TextField::new('title'),
+            TextareaField::new('description'),
+            DateTimeField::new('createdAt')->onlyOnIndex(),
+        ];
+    }
+    
+    /*
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id'),
+            TextField::new('title'),
+            TextEditorField::new('description'),
+        ];
+    }
+    */
+}
